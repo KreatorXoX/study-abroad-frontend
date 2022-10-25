62610d307453f9ed4242175b0b6752251e022217
Gorkem Gocer <gorkemgocer7@gmail.com>
2022-10-25 13:44:12 +0300

    re-designed and added slider

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,26 +1,24 @@
 import React from "react";
-import Input from "../shared/components/Form-Elements/Input";
+
 import Button from "../shared/components/Form-Elements/Button";
-import { VALIDATOR_EMAIL, VALIDATOR_REQUIRE } from "../shared/utils/validators";
+import SidePicture from "../assets/images/bg.jpg";
 import styles from "./Landing.module.css";
+import Slider from "../shared/components/UI-Elements/Slider";
 const Landing = () => {
   return (
-    <div className={styles.backgroundImage}>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <div className={styles.innerContent}>
-            <h3 className={styles.cto}>
-              <span className={styles.highlight}>Hayalindeki</span> Ulkede
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.innerContent}>
+          <div>
+            <h1 className={styles.cto}>
+              <div className={styles.highlight}>Thou Shall Not Pass</div>
               <br />
-              <span className={styles.highlight}>
-                Dunya
-              </span> Standartlarinda <br />
-              <span className={styles.highlight}>Egitim</span> Firsatini
-              Yakalamaya
-              <br />
-              <span className={styles.highlight}>Hazir</span> Misiniz ?
-            </h3>
-
+              <span className={styles.color}>The Best Consultants</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
             <div className={styles.actions}>
               <Button large to="/countries">
                 Explore Now
@@ -28,45 +26,49 @@ const Landing = () => {
             </div>
           </div>
         </div>
-        <div className={styles.content}>
-          <form className={styles.form}>
-            <h3>Giris Yap</h3>
-            <div className={styles["form-control"]}>
-              <Input
-                id="email"
-                type="email"
-                //label="Email"
-                placeholder="Email : ex@example.com"
-                v
-                errorText="Enter a valid email"
-                onInputChange={() => {}}
-                validators={[VALIDATOR_EMAIL()]}
-              />
-            </div>
-            <div className={styles["form-control"]}>
-              <Input
-                id="password"
-                type="password"
-                // label="Password"
-                placeholder="Password"
-                errorText="Enter a valid password"
-                onInputChange={() => {}}
-                validators={[VALIDATOR_REQUIRE()]}
-              />
-            </div>
-            <div className={styles["form-action"]}>
-              <Button type="submit" mid>
-                Login
-              </Button>
-            </div>
-          </form>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.sidePic}>
+          <img src={SidePicture} alt="homepage" />
         </div>
-        <div className={styles.bottomContent}>
-          <h3>Careousel</h3>
-        </div>
+      </div>
+      <div className={styles.slider}>
+        <Slider />
       </div>
     </div>
   );
 };
 
 export default Landing;
+
+//  <form className={styles.form}>
+//    <h3>Giris Yap</h3>
+//    <div className={styles["form-control"]}>
+//      <Input
+//        id="email"
+//        type="email"
+//        //label="Email"
+//        placeholder="Email : ex@example.com"
+//        v
+//        errorText="Enter a valid email"
+//        onInputChange={() => {}}
+//        validators={[VALIDATOR_EMAIL()]}
+//      />
+//    </div>
+//    <div className={styles["form-control"]}>
+//      <Input
+//        id="password"
+//        type="password"
+//        // label="Password"
+//        placeholder="Password"
+//        errorText="Enter a valid password"
+//        onInputChange={() => {}}
+//        validators={[VALIDATOR_REQUIRE()]}
+//      />
+//    </div>
+//    <div className={styles["form-action"]}>
+//      <Button type="submit" mid>
+//        Login
+//      </Button>
+//    </div>
+//  </form>;
