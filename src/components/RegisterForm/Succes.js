92b7102d9a98834d4d7d842b94997fe65635b3e2
Gorkem Gocer <gorkemgocer7@gmail.com>
2022-10-30 20:20:08 +0300

    Ready for new branching

--- src/components/RegisterForm/Succes.js
+++ src/components/RegisterForm/Succes.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Success from "../../assets/images/success.jpg";
 const Succes = () => {
+  const history = useHistory();
   return (
     <>
       <h2 style={{ textAlign: "center", gridArea: "form", color: "#94C973" }}>
         Confirmation Successful
       </h2>
-      <p>
+      <p
+        onClick={() => {
+          history.push("/countries");
+        }}
+      >
         You are one step closer to your{" "}
         <span style={{ color: "red" }}>Dreams</span>
       </p>
