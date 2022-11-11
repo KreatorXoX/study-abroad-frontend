fe2ea83991a5c606c9e287c50af65425b77e69b0
KreatorXoX <gorkemgocer7@gmail.com>
2022-11-11 16:37:04 +0300

    finished university page

--- src/pages/University.js
+++ src/pages/University.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { useParams, useHistory } from "react-router-dom";
-import { useForm } from "../hooks/form-hook";
-import Input from "../shared/components/Form-Elements/Input";
-import Button from "../shared/components/Form-Elements/Button";
-import { data } from "../dummyData/countries";
-import styles from "./University.module.css";
+import React from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { useForm } from '../hooks/form-hook'
+import Input from '../shared/components/Form-Elements/Input'
+import Button from '../shared/components/Form-Elements/Button'
+import { data } from '../dummyData/countries'
+import styles from './University.module.css'
 
 const University = () => {
-  const history = useHistory();
-  const uid = useParams().uid;
-  const cid = useParams().cid;
-  const country = data.countries.find((c) => c.id === cid);
-  const university = country.schools.find((u) => u.id === uid);
+  const history = useHistory()
+  const uid = useParams().uid
+  const cid = useParams().cid
+  const country = data.countries.find(c => c.id === cid)
+  const university = country.schools.find(u => u.id === uid)
 
   return (
     <div className={styles.rows}>
@@ -19,9 +19,14 @@ const University = () => {
         <div className={styles.col1}>
           <div>
             <h2>{university.name}</h2>
-            <h2>Cui servire regnare est</h2>
+            <h3>Cui servire regnare est</h3>
             <div className={styles.searchUni}>
-              <p>Description</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore
+                asperiores totam in omnis vel amet ea molestiae, soluta tenetur
+                deserunt magni ratione fugit a? Laudantium libero nemo fugiat
+                labore sit.
+              </p>
             </div>
           </div>
         </div>
@@ -29,12 +34,12 @@ const University = () => {
           <div>
             <iframe
               className={styles.video}
-              width="80%"
-              height="100%"
-              src="https://www.youtube.com/embed/gfUSAEJINRY"
-              title="BRUSSELS City Tour / Belgium"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              width='80%'
+              height='100%'
+              src='https://www.youtube.com/embed/gfUSAEJINRY'
+              title='BRUSSELS City Tour / Belgium'
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
               allowFullScreen
             ></iframe>
           </div>
@@ -45,13 +50,36 @@ const University = () => {
         </div>
       </div>
       <div className={styles.row2}>
-        <div className={styles.item}>education</div>
-        <div className={styles.item}>research</div>
-        <div className={styles.item}>engagement</div>
-        <div className={styles.item}>engagement</div>
+        <div className={`${styles.item} ${styles.item1}`}>
+          <div className={styles.overlay}>
+            <h5 className={styles.overlayTitle}>education</h5>
+            <h6 className={styles.overlayDescription}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Temporibus recusandae
+            </h6>
+          </div>
+        </div>
+        <div className={`${styles.item} ${styles.item2}`}>
+          <div className={styles.overlay}>
+            <h5 className={styles.overlayTitle}>research</h5>
+            <h6 className={styles.overlayDescription}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Temporibus recusandae
+            </h6>
+          </div>
+        </div>
+        <div className={`${styles.item} ${styles.item3}`}>
+          <div className={styles.overlay}>
+            <h5 className={styles.overlayTitle}>engage</h5>
+            <h6 className={styles.overlayDescription}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Temporibus recusandae
+            </h6>
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default University;
+export default University
