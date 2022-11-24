a55439030f7447cd93638172b0a686b2fdad81c5
Gorkem Gocer <gorkemgocer7@gmail.com>
2022-11-24 16:45:47 +0300

    added react-query

--- src/components/Cms/EmployeesList.jsx
+++ src/components/Cms/EmployeesList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import { useQuery } from "react-query";
+import { getUsersByRole } from "../../api/usersApi";
 import Button from "../../shared/components/Form-Elements/Button";
 import Card from "../../shared/components/UI-Elements/Card";
 import SearchBar from "../../shared/components/UI-Elements/SearchBar";
@@ -10,7 +11,25 @@ import { employees } from "../../dummyData/employees";
 import styles from "./UserList.module.css";
 const EmployeesList = () => {
   const [showForm, setShowForm] = useState(false);
+  const role = "employee";
+  const {
+    data: emps,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(["employees", role], getUsersByRole, {
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+  });
 
+  if (isError) {
+    return <p>Error</p>;
+  }
+
+  if (isLoading) {
+    return <p>...Loading</p>;
+  }
+  console.log(emps);
   return (
     <div className={styles.listPage}>
       {showForm && (
@@ -35,10 +54,10 @@ const EmployeesList = () => {
             </Button>
           </div>
           <div className={styles.list}>
-            {employees.map((emp) => (
+            {emps.map((emp) => (
               <Card
-                key={emp.id}
-                name={emp.name}
+                key={emp._id}
+                name={emp.username}
                 image={
                   "https://img.freepik.com/free-photo/studio-portrait-bearded-man-posing-beige-background-looking-into-camera-with-broad-smile-his-face_295783-16582.jpg?w=1380&t=st=1668962600~exp=1668963200~hmac=1d23c2110f4ae876f45c91e50b56163a351f03060a154d846db3639ed676e04c"
                 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
